06670f7f7b128a221ffb7efe1ebafc258df39c3b
Roman Habitzl <Roman.Habitzl@gmx.net>
2021-01-02 21:05:20 +0100

    - add unit test for ReportBasedStatusMonitor

---
+++ src/test/java/app/habitzl/elasticsearch/status/monitor/tool/ReportBasedStatusMonitorTest.java
@@ -0,0 +1,41 @@
+package app.habitzl.elasticsearch.status.monitor.tool;
+
+import app.habitzl.elasticsearch.status.monitor.AnalysisReports;
+import app.habitzl.elasticsearch.status.monitor.tool.analysis.data.AnalysisReport;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class ReportBasedStatusMonitorTest {
+
+	private ReportBasedStatusMonitor sut;
+	private StatusAnalyser analyser;
+	private ReportGenerator reportGenerator;
+
+	@BeforeEach
+	void setUp() {
+		analyser = mock(StatusAnalyser.class);
+		reportGenerator = mock(ReportGenerator.class);
+		sut = new ReportBasedStatusMonitor(analyser, reportGenerator);
+	}
+
+	@Test
+	void createSnapshot_analyserReturnsReportModel_generatesReport() {
+		// Given
+		AnalysisReport report = prepareAnalyser();
+
+		// When
+		sut.createSnapshot();
+
+		// Then
+		verify(analyser).createReport();
+		verify(reportGenerator).generate(report);
+	}
+
+	private AnalysisReport prepareAnalyser() {
+		AnalysisReport report = AnalysisReports.random();
+		when(analyser.createReport()).thenReturn(report);
+		return report;
+	}
+}
