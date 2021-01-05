33a19a8baad0a75ef973566d048a7f68693de78a
RHabitzl <Roman.Habitzl@frequentis.com>
2021-01-05 09:50:59 +0100

    - improve HTML report formatting (use tables instead of paragraphs) - change time format to [d]d [hh]h [mm]min

--- src/main/java/app/habitzl/elasticsearch/status/monitor/tool/client/mapper/format/DayBasedTimeFormatter.java
+++ src/main/java/app/habitzl/elasticsearch/status/monitor/tool/client/mapper/format/DayBasedTimeFormatter.java
@@ -4,10 +4,10 @@ import app.habitzl.elasticsearch.status.monitor.tool.client.mapper.TimeFormatter
 import java.time.Duration;
 
 /**
- * Implementation of the {@link TimeFormatter} that formats in [days]:[hours]:[minutes].
+ * Implementation of the {@link TimeFormatter} that formats time as {@code [days]d [hours]h [minutes]min}.
  */
 public class DayBasedTimeFormatter implements TimeFormatter {
-    private static final String DURATION_FORMAT = "%d:%02d:%02d";
+    private static final String DURATION_FORMAT = "%dd %02dh %02dmin";
 
     @Override
     public String format(final Duration duration) {
--- src/test/java/app/habitzl/elasticsearch/status/monitor/tool/client/mapper/format/DayBasedTimeFormatterTest.java
+++ src/test/java/app/habitzl/elasticsearch/status/monitor/tool/client/mapper/format/DayBasedTimeFormatterTest.java
@@ -1,52 +1,51 @@
 package app.habitzl.elasticsearch.status.monitor.tool.client.mapper.format;
 
+import java.time.Duration;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.time.Duration;
-import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 class DayBasedTimeFormatterTest {
 
-	private DayBasedTimeFormatter sut;
+    private DayBasedTimeFormatter sut;
 
-	@BeforeEach
-	void setUp() {
-		sut = new DayBasedTimeFormatter();
-	}
+    @BeforeEach
+    void setUp() {
+        sut = new DayBasedTimeFormatter();
+    }
 
-	@ParameterizedTest
-	@MethodSource(value = "durationToString")
-	void format_duration_returnsFormattedString(final Duration duration, final String expectedString) {
-		// Given
-		// duration as parameter
+    @ParameterizedTest
+    @MethodSource(value = "durationToString")
+    void format_duration_returnsFormattedString(final Duration duration, final String expectedString) {
+        // Given
+        // duration as parameter
 
-		// When
-		String result = sut.format(duration);
+        // When
+        String result = sut.format(duration);
 
-		// Then
-		assertThat(result, equalTo(expectedString));
-	}
+        // Then
+        assertThat(result, equalTo(expectedString));
+    }
 
-	private static Stream<Arguments> durationToString() {
-		return Stream.of(
-				Arguments.of(Duration.ZERO, "0:00:00"),
-				Arguments.of(Duration.ofSeconds(59), "0:00:00"),
-				Arguments.of(Duration.ofMinutes(1), "0:00:01"),
-				Arguments.of(Duration.ofMinutes(5), "0:00:05"),
-				Arguments.of(Duration.ofMinutes(30), "0:00:30"),
-				Arguments.of(Duration.ofMinutes(90), "0:01:30"),
-				Arguments.of(Duration.ofMinutes(1501), "1:01:01"),
-				Arguments.of(Duration.ofHours(1), "0:01:00"),
-				Arguments.of(Duration.ofHours(12), "0:12:00"),
-				Arguments.of(Duration.ofHours(27), "1:03:00"),
-				Arguments.of(Duration.ofDays(1), "1:00:00"),
-				Arguments.of(Duration.ofDays(142), "142:00:00")
-		);
-	}
+    private static Stream<Arguments> durationToString() {
+        return Stream.of(
+                Arguments.of(Duration.ZERO, "0d 00h 00min"),
+                Arguments.of(Duration.ofSeconds(59), "0d 00h 00min"),
+                Arguments.of(Duration.ofMinutes(1), "0d 00h 01min"),
+                Arguments.of(Duration.ofMinutes(5), "0d 00h 05min"),
+                Arguments.of(Duration.ofMinutes(30), "0d 00h 30min"),
+                Arguments.of(Duration.ofMinutes(90), "0d 01h 30min"),
+                Arguments.of(Duration.ofMinutes(1501), "1d 01h 01min"),
+                Arguments.of(Duration.ofHours(1), "0d 01h 00min"),
+                Arguments.of(Duration.ofHours(12), "0d 12h 00min"),
+                Arguments.of(Duration.ofHours(27), "1d 03h 00min"),
+                Arguments.of(Duration.ofDays(1), "1d 00h 00min"),
+                Arguments.of(Duration.ofDays(142), "142d 00h 00min")
+        );
+    }
 }
